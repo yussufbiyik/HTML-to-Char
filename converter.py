d56887d836b1095d6175239948ddfd1561dfc07d
Yusuf Bıyık <yusufbyk3660@gmail.com>
2020-06-11 19:31:05 +0300

    ⚙ Add Arguments
    This was input and output name can be changed.

--- converter.py
+++ converter.py
@@ -1,12 +1,28 @@
-import re
+import re, argparse
+
+
+parser = argparse.ArgumentParser(description='Convert a html file to char variables for NodeMCU WebServers.')
+parser.add_argument('-f', '--file', type=str, metavar='', help='File name including file format. Default is index.html.')
+parser.add_argument('-o', '--out', type=str, metavar='', help='File name of output. Default is result.txt.')
+args = parser.parse_args()
+
+if isinstance(args.file, str):
+    filename = args.file
+else:
+    filename = "index.html"
+if isinstance(args.out, str):
+    outfilename = args.out
+else:
+    outfilename = "result.txt"
+    
 
 try:
-    targetFile = open("index.txt","a+")
+    targetFile = open(filename,"a+")
     targetFile.seek(0)
     fileLength = sum(1 for line in targetFile)
     targetFile.seek(0)
     
-    result = open("result.txt","w+")
+    result = open(outfilename,"w+")
     x=1
     result.write("char *html = ")
     for line in targetFile:
